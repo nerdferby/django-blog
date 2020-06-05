f03b117ecbb3d9c9a32fef5b0d0b89f5037d5ed7
Scott Cummins (Ferby) <scottcummins3356@gmail.com>
2020-06-05 02:54:19 +0100

    Finishing part 8

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'  # researching for beyond bootstrap
 
 LOGIN_REDIRECT_URL = "blog-home"
